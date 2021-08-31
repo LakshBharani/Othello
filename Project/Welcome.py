b193f103a4a9fb836824f7697f3ebca1ad534589
LakshBharani <laksh.bharani@gmail.com>
2021-08-31 18:32:46 +0530

    Welcome Page
    This page is the 1st screen visible to the player.

---
+++ Project/Welcome.py
@@ -0,0 +1,20 @@
+print('''
+            Welcome to Othello
+            ******************
+
+            ##################
+            #     Easy(1)    #
+            ##################
+
+            ##################
+            #     Hard(2)    #
+            ##################
+''')
+diff = int(input("Enter difficulty level(1/2): "))
+
+if diff == 1:
+    print("Loading...")
+    import gameEz
+elif diff == 2:
+    print("Loading...")
+    import gameHard
